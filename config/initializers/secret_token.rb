9f12027a6c55a11acfb9335b4509dfe779926486
Joel Ojeda <ojeda.joel@gmail.com>
2013-04-22 20:53:34 -0430

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joelojeda::Application.config.secret_token = 'c8d2936bab1f0536d723f6dfaa498e12b5d758fb6a52c76b2853ae0d1e074491a424926ede287ff9489ab3f10867f6fb92c165f729e9020c8010a8088e9fad8c'
